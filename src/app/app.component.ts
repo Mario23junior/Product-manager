c47cb070b72eb809f9a5688a5162bf30b3c2a303
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-27 15:37:58 -0300

    removendo template padrao e inserindo template
    removendo template padrao e inserindo template customizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: 'app.component.html',
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  title = 'produto-gerence';
-}
+  
+ }
