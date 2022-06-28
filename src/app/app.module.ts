53d6fd3bcee0fffa5797fe1c989e0207ace72fb2
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-28 08:44:25 -0300

    inserindo toobarModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar'
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+ 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent
-  ],
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
