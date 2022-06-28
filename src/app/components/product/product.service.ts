71202574d60feb668c188424301c9ad1de9a5092
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-28 11:38:18 -0300

    inserindo snackbar

---
+++ src/app/components/product/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private snackBar: MatSnackBar
+
+  constructor(snackBar: MatSnackBar) {
+    this.snackBar = snackBar
+  }
+
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, 'x', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    })
+  }
+}
